7558bbc4eb14d5780dc646582110a4f7dfe6ccb3
Kyle Lohrberg <lorbs32@gmail.com>
2025-07-25 21:54:36 -0400

    initializing spring boot setup and git

---
+++ src/main/java/dev/kylelohrberg/adminflow/AdminFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.kylelohrberg.adminflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kylelohrberg/adminflow/AdminFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kylelohrberg.adminflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
